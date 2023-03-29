e9c5a310391a3569e0d9d78ad31da66ae826de7f
cebalc <eric.ceballos.alcantarilla@gmail.com>
2023-03-29 21:11:27 +0200

    Instalados nuevos paquetes y cambiado componente App a clase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [data, setData] = React.useState(null);
+interface AppProps {
+}
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((response) => response.json())
-      .then((data) => setData(data.message));
-  }, []);
+interface AppState {
+  data: any
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+class App extends React.Component<AppProps, AppState> {
+  public state: AppState = {
+    data: null
+  }
+
+  public constructor(props: any) {
+    super(props);
+  }
+
+  public async componentDidMount(): Promise<void> {
+    try {
+      let response: Response = await fetch("/api");
+      console.log(response);
+      let jsonData = await response.json();
+      console.log(jsonData);
+      this.setState({data: jsonData.message});
+    } catch (err) {
+      // this.setState({data: `Error: ${err}`})
+      console.log(err);
+    }
+  }
+
+  public render(): React.ReactNode {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>{!this.state.data ? "Loading..." : this.state.data}</p>
+          </header>
+        </div>
+      );
+  }
 }
 
 export default App;
