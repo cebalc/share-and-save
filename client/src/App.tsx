ff6489e55527257ba50dd7c884731bb6da234ace
cebalc <eric.ceballos.alcantarilla@gmail.com>
2023-03-31 19:46:01 +0200

    Eliminados ficheros no esenciales y creadas estructuras de directorios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 interface AppProps {
 }
 
 interface AppState {
-  data: any
 }
 
 class App extends React.Component<AppProps, AppState> {
   public state: AppState = {
-    data: null
   }
 
   public constructor(props: any) {
     super(props);
   }
 
-  public async componentDidMount(): Promise<void> {
-    try {
-      let response: Response = await fetch("/api");
-      console.log(response);
-      let jsonData = await response.json();
-      console.log(jsonData);
-      this.setState({data: jsonData.message});
-    } catch (err) {
-      // this.setState({data: `Error: ${err}`})
-      console.log(err);
-    }
-  }
-
   public render(): React.ReactNode {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>{!this.state.data ? "Loading..." : this.state.data}</p>
-          </header>
-        </div>
-      );
+    return (
+      <></>
+    );
   }
 }
 
